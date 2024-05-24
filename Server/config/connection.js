92749a86cb82e46d421ed78f1a8e9d3d848afee7
Josh Roth <raider4414@gmail.com>
2024-05-24 01:55:08 -0500

    mongo db connection

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/invoicinator');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://raider4414:Ylofq9q8!@cluster0.3j6z64z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
