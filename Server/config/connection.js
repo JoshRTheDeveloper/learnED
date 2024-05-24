9ac31524680896abc0e5fe5e4818a42faf0c8a67
Josh Roth <raider4414@gmail.com>
2024-05-24 02:09:08 -0500

    updated

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://raider4414:Ylofq9q8!@cluster0.3j6z64z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
