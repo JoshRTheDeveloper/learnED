5b8099e83f5e2854fa8b7a9f796db24609a02594
Josh Roth <raider4414@gmail.com>
2024-05-24 02:29:45 -0500

    connection update

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+// Check if MONGODB_URI environment variable is set
+if (!process.env.MONGODB_URI) {
+    console.error('MONGODB_URI environment variable is not set.');
+    process.exit(1); // Exit process if MONGODB_URI is not set
+}
+
+// Connect to MongoDB using the provided URI
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error.message);
+    });
 
 module.exports = mongoose.connection;
