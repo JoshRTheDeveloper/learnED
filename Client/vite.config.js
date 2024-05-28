3af8cdc831c22f9a0db1f6c77003ace058916a47
Josh Roth <raider4414@gmail.com>
2024-05-28 07:16:13 -0500

    more PWA adjustments

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
@@ -10,31 +10,33 @@ export default defineConfig({
       manifest: {
         name: 'Invoicinator',
         short_name: 'Invoicinator',
-        description: 'A simple app for invoices',
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
+        lang: 'en',
+        scope: '/',
+        description: 'A simple app for invoices',
         theme_color: '#000000',
         icons: [
           {
-            src: 'src/assets/invoicinator192.png',
+            src: '/assets/invoicinator192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'src/assets/invoicinator512.png',
+            src: '/assets/invoicinator512.png',
             sizes: '512x512',
             type: 'image/png'
           }
         ],
         screenshots: [
           {
-            src: 'src/assets/longScreenshot.png',
+            src: '/assets/longScreenshot.png',
             sizes: '1080x1920',
             type: 'image/png'
           },
           {
-            src: 'src/assets/screenshot1.png',
+            src: '/assets/screenshot1.png',
             sizes: '1920x1080',
             type: 'image/png',
             form_factor: 'wide'
@@ -54,8 +56,18 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
   test: {
     globals: true,
     environment: 'happy-dom'
   }
-})
+});
