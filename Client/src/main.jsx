42dec670c5760a58855ff39d8ee1736fbb75eac3
Josh Roth <raider4414@gmail.com>
2024-06-04 13:23:59 -0500

    added new pathing in manifest

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -39,6 +39,9 @@ const router = createBrowserRouter([
   }
 ])
 
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 )
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      base: './',
-      
       manifest: {
         name: 'Invoicinator',
         short_name: 'Invoicinator',
@@ -20,24 +18,24 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
              {
-      "src": "https://invoicinator3000-d580657ecca9.herokuapp.com/assets/invoicinator192.png",
+      "src": "/assets/invoicinator192.png",
       "sizes": "192x192",
       "type": "image/png"
     },
     {
-      "src": "https://invoicinator3000-d580657ecca9.herokuapp.com/assets/invoicinator512.png",
+      "src": "/assets/invoicinator512.png",
       "sizes": "512x512",
       "type": "image/png"
     }
   ],
   screenshots: [
     {
-      "src": "https://invoicinator3000-d580657ecca9.herokuapp.com/assets/longScreenshot.png",
+      "src": "/assets/longScreenshot.png",
       "sizes": "1080x1920",
       "type": "image/png"
     },
     {
-      "src": "https://invoicinator3000-d580657ecca9.herokuapp.com/assets/screenshot1.png",
+      "src": "/assets/screenshot1.png",
       "sizes": "1920x1080",
       "type": "image/png",
       "form_factor": "wide"
