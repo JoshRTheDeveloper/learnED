e7fe617cdaf49c0c1b68211712f960ac6fb1199b
Josh Roth <raider4414@gmail.com>
2024-06-06 01:30:17 -0500

    serviceworker

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -40,6 +40,17 @@ const router = createBrowserRouter([
   }
 ]);
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+          .then(registration => {
+              console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          })
+          .catch(error => {
+              console.log('ServiceWorker registration failed: ', error);
+          });
+  });
+}
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
