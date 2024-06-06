9ea0bdc5f5409441dfce55950588b2e579791527
Josh Roth <raider4414@gmail.com>
2024-06-06 01:19:27 -0500

    scope updated

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -40,17 +40,6 @@ const router = createBrowserRouter([
   }
 ]);
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-      navigator.serviceWorker.register('../client/dist/sw.js', { scope: '/' })
-          .then(registration => {
-              console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          })
-          .catch(error => {
-              console.log('ServiceWorker registration failed: ', error);
-          });
-  });
-}
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
