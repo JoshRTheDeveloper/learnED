c53dd0ca5abac225f4f0cad72cebf09c9da752ff
Josh Roth <raider4414@gmail.com>
2024-06-06 13:21:07 -0500

    added

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-      navigator.serviceWorker.register('../client/dist/sw.js')
+      navigator.serviceWorker.register('../client/dist/sw.js', { scope: '/' })
           .then(registration => {
               console.log('ServiceWorker registration successful with scope: ', registration.scope);
           })
