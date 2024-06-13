e4e4b3c210b65774bc017c27169e992ce41fc036
Josh Roth <raider4414@gmail.com>
2024-06-13 10:43:28 -0500

    updated

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './styles.css';
-
+require('dotenv').config();
 import App from './App.jsx';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard.jsx';
