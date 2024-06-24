8618bf4648d16c5bef0d7851b57ebccfa2da6d82
Josh Roth <raider4414@gmail.com>
2024-06-24 12:36:48 -0500

    profile

--- Client/src/pages/profile.jsx
+++ Client/src/pages/profile.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import jwtDecode from 'jwt-decode';
 import './profile.css';
 import Sidebar from '../components/sidebar/sidebar';
 import temporaryImage from '../assets/noLogo.svg';
+import axios from 'axios';
+import { GET_USER } from '../utils/queries';
 import {
+  storeUserData,
   storeProfilePicture,
   storeProfileFile,
   getUserData,
   getProfilePicture,
+  getProfileFile,
 } from '../utils/indexedDB';
+import {
+  CHANGE_COMPANY,
+  CHANGE_PROFILE_PICTURE,
+  CHANGE_STREET_ADDRESS,
+  CHANGE_EMAIL,
+  CHANGE_CITY,
+  CHANGE_STATE,
+  CHANGE_ZIP,
+} from '../utils/mutations';
 
 const Profile = () => {
   const [userData, setUserData] = useState(null);
@@ -27,12 +42,46 @@ const Profile = () => {
   const decodedToken = jwtDecode(token);
   const userId = decodedToken.data._id;
 
+  const { loading, data, refetch } = useQuery(GET_USER, {
+    variables: { userId: userId || '' },
+    skip: !navigator.onLine || !initialLoad,
+  });
+
+  const [changeCompanyMutation] = useMutation(CHANGE_COMPANY);
+  const [changeProfilePictureMutation] = useMutation(CHANGE_PROFILE_PICTURE);
+  const [changeStreetAddressMutation] = useMutation(CHANGE_STREET_ADDRESS);
+  const [changeEmailMutation] = useMutation(CHANGE_EMAIL);
+  const [changeCityMutation] = useMutation(CHANGE_CITY);
+  const [changeStateMutation] = useMutation(CHANGE_STATE);
+  const [changeZipMutation] = useMutation(CHANGE_ZIP);
+
+  useEffect(() => {
+    const handleOnlineStatusChange = async () => {
+      const isOnline = navigator.onLine;
+      setOfflineMode(!isOnline);
+
+      if (isOnline) {
+        await syncOfflineData();
+        refetch();
+      } else {
+        console.error('Went offline.');
+      }
+    };
+
+    window.addEventListener('online', handleOnlineStatusChange);
+    window.addEventListener('offline', handleOnlineStatusChange);
+
+    return () => {
+      window.removeEventListener('online', handleOnlineStatusChange);
+      window.removeEventListener('offline', handleOnlineStatusChange);
+    };
+  }, [refetch]);
+
   useEffect(() => {
     const fetchData = async () => {
       if (initialLoad) {
         try {
           let userDataFromDB;
-
 
           if (navigator.onLine && !loading && data && data.getUser) {
             userDataFromDB = data.getUser;
@@ -49,15 +98,7 @@ const Profile = () => {
             setZip(zip);
             setUserData(userDataFromDB);
             setCompany(company);
-
-            // Set profile picture from IndexedDB Blob
-            if (profilePicture) {
-              const pictureBlob = await getProfilePicture(userId);
-              const blobUrl = URL.createObjectURL(pictureBlob);
-              setLogoUrl(blobUrl);
-            } else {
-              setLogoUrl(temporaryImage);
-            }
+            setLogoUrl(profilePicture || temporaryImage);
           } else {
             console.error('No offline data found.');
           }
@@ -70,7 +111,60 @@ const Profile = () => {
     };
 
     fetchData();
-  }, [userId, initialLoad]);
+  }, [loading, data, userId, initialLoad]);
+
+  const syncOfflineData = async () => {
+    try {
+      const offlineUserData = await getUserData(userId);
+      const offlineProfilePictureBlob = await getProfilePicture(userId);
+      const offlineProfilePictureFile = await getProfileFile(userId);
+
+      if (offlineUserData) {
+        const { company, email, streetAddress, city, state, zip } = offlineUserData;
+        let picturePath = offlineUserData.profilePicture;
+
+        if (offlineProfilePictureFile) {
+          const uploadedPicturePath = await uploadProfilePicture(offlineProfilePictureFile);
+          picturePath = uploadedPicturePath;
+        }
+
+        await Promise.all([
+          changeCompanyMutation({ variables: { userId, company } }),
+          changeStreetAddressMutation({ variables: { userId, streetAddress } }),
+          changeEmailMutation({ variables: { userId, email } }),
+          changeCityMutation({ variables: { userId, city } }),
+          changeStateMutation({ variables: { userId, state } }),
+          changeZipMutation({ variables: { userId, zip } }),
+          changeProfilePictureMutation({
+            variables: { userId, profilePicture: picturePath },
+          }),
+        ]);
+
+        setUserData({
+          ...offlineUserData,
+          profilePicture: picturePath,
+        });
+
+        setCompany(offlineUserData.company);
+        setEmail(offlineUserData.email);
+        setStreetAddress(offlineUserData.streetAddress);
+        setCity(offlineUserData.city);
+        setState(offlineUserData.state);
+        setZip(offlineUserData.zip);
+        setLogoUrl(picturePath || temporaryImage);
+
+        await storeUserData({
+          ...offlineUserData,
+          profilePicture: picturePath,
+        });
+
+      } else {
+        console.error('No offline changes to sync.');
+      }
+    } catch (error) {
+      console.error('Error syncing data with server:', error);
+    }
+  };
 
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handleStreetAddressChange = (e) => setStreetAddress(e.target.value);
@@ -81,18 +175,31 @@ const Profile = () => {
 
   const handleLogoChange = async (e) => {
     const file = e.target.files[0];
-    if (!file) return;
-
     const blobUrl = URL.createObjectURL(file);
     setLogo(file);
     setLogoUrl(blobUrl);
-
     const filename = `${userId}_profile_picture.jpg`;
     const renamedFile = new File([file], filename, { type: file.type });
     setRenamedFile(renamedFile);
-
     await storeProfilePicture(userId, file); 
     await storeProfileFile(userId, renamedFile); 
+  };
+
+  const uploadProfilePicture = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+      const response = await axios.post('https://invoicinator3000-d580657ecca9.herokuapp.com/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'userId': userId,
+        },
+      });
+      return response.data.fileUrl;
+    } catch (error) {
+      console.error('Error uploading profile picture:', error);
+      throw error;
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -108,7 +215,6 @@ const Profile = () => {
           await storeProfilePicture(userId, picturePath); 
         }
 
-    
         await Promise.all([
           changeCompanyMutation({ variables: { userId, company } }),
           changeStreetAddressMutation({ variables: { userId, streetAddress } }),
@@ -119,7 +225,6 @@ const Profile = () => {
           changeProfilePictureMutation({ variables: { userId, profilePicture: picturePath } }),
         ]);
 
- 
         setUserData({
           userId,
           email,
@@ -142,7 +247,6 @@ const Profile = () => {
           profilePicture: picturePath,
         });
       } else {
-        // Store offline changes to IndexedDB
         const offlineUserData = {
           userId,
           email,
@@ -157,12 +261,11 @@ const Profile = () => {
         await storeUserData(offlineUserData);
 
         if (logo) {
-          await storeProfilePicture(userId, logoUrl); 
-          await storeProfileFile(userId, renamedFile); 
+          await storeProfilePicture(userId, logoUrl); // Save blob for offline use
+          await storeProfileFile(userId, renamedFile); // Save file for later upload
         }
       }
 
-      // Reset form state after submission
       setEmail(email);
       setStreetAddress(streetAddress);
       setCity(city);
