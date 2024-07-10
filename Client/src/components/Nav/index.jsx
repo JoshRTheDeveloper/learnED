0e2e11aaad6b9ab234916fab814c20014d3e1246
Josh Roth <raider4414@gmail.com>
2024-07-10 01:54:39 -0500

    updated nav

--- Client/src/components/Nav/index.jsx
+++ Client/src/components/Nav/index.jsx
@@ -5,66 +5,55 @@ import Logo from "../../assets/Logo.svg";
 import SignupModal from '../signup-modal/modal';
 import LoginModal from '../login-modal/login-modal';
 import { useNavigate } from "react-router-dom";
-
-import './index.css'
+import { useApolloClient } from '@apollo/client';
+import { clearOfflineMutations, getOfflineMutations } from '../../utils/indexedDB'; 
+import { CREATE_INVOICE } from '../../utils/mutations';
+import './index.css';
 
 function Nav() {
   const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
-
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false); 
   const [firstName, setFirstName] = useState("");
-  const [onlineStatus, setOnlineStatus] = useState(navigator.onLine ? 'Online' : 'Offline'); 
-
+  const [onlineStatus, setOnlineStatus] = useState(navigator.onLine ? 'Online' : 'Offline');
   const navigate = useNavigate();
+  const client = useApolloClient();
 
   useEffect(() => {
     if (Auth.loggedIn()) {
       fetchFirstName();
     }
-  
 
-  const handleOnlineStatusChange = () => {
-    setOnlineStatus(navigator.onLine ? 'Online' : 'Offline');
-  };
+    const handleOnlineStatusChange = () => {
+      setOnlineStatus(navigator.onLine ? 'Online' : 'Offline');
+    };
 
-  window.addEventListener('online', handleOnlineStatusChange);
-  window.addEventListener('offline', handleOnlineStatusChange);
+    window.addEventListener('online', handleOnlineStatusChange);
+    window.addEventListener('offline', handleOnlineStatusChange);
 
-
-  return () => {
-    window.removeEventListener('online', handleOnlineStatusChange);
-    window.removeEventListener('offline', handleOnlineStatusChange);
-  };
-}, []);
-
+    return () => {
+      window.removeEventListener('online', handleOnlineStatusChange);
+      window.removeEventListener('offline', handleOnlineStatusChange);
+    };
+  }, []);
 
   const fetchFirstName = async () => {
-    const user = await Auth.getProfile();
-    if (user && user.data) {
-      setFirstName(user.data.firstName);
+    try {
+      const user = await Auth.getProfile();
+      if (user && user.data) {
+        setFirstName(user.data.firstName);
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
     }
   };
 
-  const navigateToServices = () => {
-    navigate("/");
-    setTimeout(() => {
-      const servicesSection = document.getElementById("services");
-      if (servicesSection) {
-        servicesSection.scrollIntoView({ behavior: "smooth" });
-      }
-    }, 500);
-  }
-
   const handleLogout = () => {
     Auth.logout();
-
     setFirstName("");
     setIsLoginModalOpen(false);
     setIsSignupModalOpen(false);
     navigate("/");
-
-
-  }
+  };
 
   const handleLoginModalClose = async () => {
     setIsLoginModalOpen(false);
@@ -74,7 +63,7 @@ function Nav() {
     }
   };
 
- const handleSignUpModalClose = async () => {
+  const handleSignUpModalClose = async () => {
     setIsSignupModalOpen(false);
     await fetchFirstName();
     if (Auth.loggedIn()) {
@@ -82,47 +71,64 @@ function Nav() {
     }
   };
 
+  const executeStoredMutations = async () => {
+    try {
+      const storedMutations = await getOfflineMutations();
+      for (const mutation of storedMutations) {
+        try {
+          const { variables } = mutation;
+          const result = await client.mutate({
+            mutation: CREATE_INVOICE, // Adjust mutation according to your setup
+            variables,
+          });
+
+          console.log('Successfully executed createInvoice mutation:', result);
+
+          await clearOfflineMutations(); // Clear all offline mutations after successful execution
+        } catch (error) {
+          console.error('Error executing stored createInvoice mutation:', error);
+        }
+      }
+    } catch (error) {
+      console.error('Failed to get offline mutations:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (navigator.onLine) {
+      executeStoredMutations();
+    }
+  }, []);
 
   if (Auth.loggedIn()) {
-
     return (
       <>
         <header>
-          <nav className="navbar navbar-expand-md navbar-light fixed-top ">
+          <nav className="navbar navbar-expand-md navbar-light fixed-top">
             <div className="container-fluid">
               <Link to="/" className="navbar-brand">
                 <img className="navbar-brand" src={Logo} alt=""  />
               </Link>
-
               <span className="online-status">Network: {onlineStatus}</span>
-
               <div className="navbar-collapse" id="navbarCollapse">
-                <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                
-                </ul>
-
+                <ul className="navbar-nav me-auto mb-2 mb-md-0"></ul>
                 <div className='mobile-container'>
-                <ul className='mobile-sidebar'>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/CreateInvoices">Create Invoice</Link></li>
-            <li><a href="/Profile">Profile</a></li>
-            <button className="mobile-logout" onClick={handleLogout}>Logout</button>
-            </ul>
-            </div>
-            <div className='width'>
-                <div className="navbar-text mx-3">
-
-                  {firstName && <span>Welcome, {firstName}! </span>} 
-               
-
+                  <ul className='mobile-sidebar'>
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li><Link to="/CreateInvoices">Create Invoice</Link></li>
+                    <li><a href="/Profile">Profile</a></li>
+                    <button className="mobile-logout" onClick={handleLogout}>Logout</button>
+                  </ul>
                 </div>
+                <div className='width'>
+                  <div className="navbar-text mx-3">
+                    {firstName && <span>Welcome, {firstName}!</span>}
+                  </div>
                 </div>
                 <ul className="navbar-nav mb-md-0">
                   <li className="nav-item">
                     <div className='logout-button'>
-                    <button className="nav-link active mx-3" onClick={handleLogout}>Logout</button>
-           
-           
+                      <button className="nav-link active mx-3" onClick={handleLogout}>Logout</button>
                     </div>
                   </li>
                 </ul>
@@ -136,45 +142,30 @@ function Nav() {
     return (
       <>
         <header>
-        <nav className="navbar navbar-expand-md navbar-light fixed-top ">
+          <nav className="navbar navbar-expand-md navbar-light fixed-top">
             <div className="container-fluid-mobile container-fluid">
               <Link to="/" className="navbar-brand-mobile navbar-brand">
                 <img className="navbar-brand-mobile navbar-brand" src={Logo} alt=""  />
               </Link>
-
-             
-
-         
-            <div className='width'>
+              <div className='width'>
                 <div className="navbar-text mx-3">
-
-                  {firstName && <span>Welcome, {firstName}! </span>} 
-
+                  {firstName && <span>Welcome, {firstName}!</span>}
                 </div>
-                </div>
-                <ul className="navbar-nav navbar-nav-mobile mb-md-0">
-               
-                  <li className="nav-item">
-                    <button className="nav-link-mobile nav-link active mx-3 small-font" onClick={() => setIsLoginModalOpen(true)}>Login</button>
-                  </li>
-                  <li className="nav-item">
-                    <button className="nav-link-mobile  nav-link active mx-3 small-font" onClick={() => setIsSignupModalOpen(true)}>Sign Up</button>
-                  </li>
-                  <span className="online-status">({onlineStatus})</span>
-                </ul>
               </div>
-        
+              <ul className="navbar-nav navbar-nav-mobile mb-md-0">
+                <li className="nav-item">
+                  <button className="nav-link-mobile nav-link active mx-3 small-font" onClick={() => setIsLoginModalOpen(true)}>Login</button>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link-mobile  nav-link active mx-3 small-font" onClick={() => setIsSignupModalOpen(true)}>Sign Up</button>
+                </li>
+                <span className="online-status">({onlineStatus})</span>
+              </ul>
+            </div>
           </nav>
-          
-          
         </header>
-
-        <SignupModal isOpen={isSignupModalOpen} onClose={handleSignUpModalClose} onCloseModal={handleSignUpModalClose} /> 
-
-       
-        <LoginModal isOpen={isLoginModalOpen} onClose={handleLoginModalClose} onCloseModal={handleLoginModalClose} /> 
-
-
+        <SignupModal isOpen={isSignupModalOpen} onClose={handleSignUpModalClose} onCloseModal={handleSignUpModalClose} />
+        <LoginModal isOpen={isLoginModalOpen} onClose={handleLoginModalClose} onCloseModal={handleLoginModalClose} />
       </>
     );
   }
