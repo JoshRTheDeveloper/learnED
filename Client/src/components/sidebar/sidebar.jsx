2dbf77fcd31298aa459c21a1fc17052202e835e5
Josh Roth <raider4414@gmail.com>
2024-07-13 07:00:08 -0500

    debugging profilepicture

--- Client/src/components/sidebar/sidebar.jsx
+++ Client/src/components/sidebar/sidebar.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import './sidebar.css'; 
-import { Link } from "react-router-dom";
-import Auth from "../../utils/auth";
-import { getUserData, getProfilePicture } from '../../utils/indexedDB'; 
+import './sidebar.css';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+import { getUserData, getProfilePicture } from '../../utils/indexedDB';
 import { GET_USER } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 
 const Sidebar = () => {
-  const [userFirstName, setUserFirstName] = useState("");
-  const [userLastName, setUserLastName] = useState("");
-  const [profilePicture, setProfilePicture] = useState("");
+  const [userFirstName, setUserFirstName] = useState('');
+  const [userLastName, setUserLastName] = useState('');
+  const [profilePicture, setProfilePicture] = useState('');
 
   const profile = Auth.getProfile();
   const userId = profile?.data?._id || '';
@@ -23,34 +23,18 @@ const Sidebar = () => {
     const fetchUserData = async () => {
       let userData, profilePicUrl;
 
-  
-      if (navigator.onLine) {
-        try {
-      
-          userData = userDataFromDB?.user;
-          profilePicUrl = userData?.profilePictureUrl;
-          
-          if (userData) {
-            setUserFirstName(userData.firstName);
-            setUserLastName(userData.lastName);
-            setProfilePicture(profilePicUrl || "");
-          }
-        } catch (error) {
-          console.error("Error fetching data from online database:", error);
-        }
-      }
-      
-      if (!navigator.onLine || !userData) {
- 
+      if (userDataFromDB && userDataFromDB.user) {
+        userData = userDataFromDB.user;
+        profilePicUrl = userData.profilePictureUrl;
+      } else {
         userData = await getUserData(userId);
-        
-        if (userData) {
-          setUserFirstName(userData.firstName);
-          setUserLastName(userData.lastName);
-          
-          profilePicUrl = await getProfilePicture(userId);
-          setProfilePicture(profilePicUrl || "");
-        }
+        profilePicUrl = await getProfilePicture(userId);
+      }
+
+      if (userData) {
+        setUserFirstName(userData.firstName);
+        setUserLastName(userData.lastName);
+        setProfilePicture(profilePicUrl || '');
       }
     };
 
@@ -59,19 +43,25 @@ const Sidebar = () => {
 
   return (
     <div className='content'>
-      <div className="sidebar">
+      <div className='sidebar'>
         <div className='profile-picture-div'>
-          {profilePicture && <img src={profilePicture} className='profile-picture2' alt="Profile" />}
+          {profilePicture && <img src={profilePicture} className='profile-picture2' alt='Profile' />}
         </div>
         {userFirstName && userLastName && <h2>Welcome, {userFirstName} {userLastName}!</h2>}
         <ul>
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/CreateInvoices">Create Invoice</Link></li>
-          <li><a href="/Profile">Profile</a></li>
+          <li>
+            <Link to='/dashboard'>Dashboard</Link>
+          </li>
+          <li>
+            <Link to='/CreateInvoices'>Create Invoice</Link>
+          </li>
+          <li>
+            <Link to='/Profile'>Profile</Link>
+          </li>
         </ul>
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
